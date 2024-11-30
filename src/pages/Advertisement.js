58175730df8a8a10b3c84fa9e7b69d2e250f7006
JunHyun <bjsd2001a@naver.com>
2024-11-30 17:09:53 +0900

    24.11.30_광고_배너_작업진행

---
+++ src/pages/Advertisement.js
@@ -0,0 +1,13 @@
+import Carosel from "../components/Carosel";
+
+
+function Advertisement(){
+
+    return(
+        <div>
+            <Carosel/>
+        </div>
+    )
+}
+
+export default Advertisement;
---
+++ src/pages/Body.js
@@ -0,0 +1,15 @@
+
+
+
+function Body(){
+
+    return(
+        <div>
+            <div>광고창 <p>취향맞춤프로젝트</p></div> {/* 슬라이더 */}
+            <div>실시간 랭킹</div> {/*  */}
+
+        </div>
+    );
+}
+
+export default Body;
