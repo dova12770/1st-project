e55395ceea9b1862ef32f84eca1abddf2e2d2009
dova12770 <aaaa2460@naver.com>
2024-12-03 10:37:56 +0900

    마이페이지1차(완성)

--- src/myPage/MyPage.js
+++ src/myPage/MyPage.js
@@ -1,36 +1,45 @@
+
 import './MyPageCss.css'
 import userImg from '../image/userImg.png'
 import { GoGear } from "react-icons/go";
 import { useState } from 'react';
-import MyPageCategory from './MyPageCategory'
-function MyPage(){
+import MyPageCategory from './MyPageCategory';
 
-    const [underLine,setUnderLine] = useState(false);
+function MyPage() {
+    const CateName = ['프로필', '후원한 프로젝트', '작성후기', '팔로워', '팔로잉'];
+    const [selectedIndex, setSelectedIndex] = useState(null);
+
     return (
         <div className='MyPage_Container'>
             <div className='Prof_Container'>
                 <div className='Avatar'>
                     <img src={userImg} alt='유저기본이미지' height='100px' width='100px'/>
                 </div>
-                <div className='UserName'>김도현 
-                <div className='OptionGear'><GoGear /></div>
-                
+                <div className='UserName'>김도현
+                    <div className='OptionGear'><GoGear /></div>
                 </div>
-                <div className='Prof_button' onClick={()=>{
-
-                }}>프로필 변경</div>
+                <div className='Prof_button' onClick={() => { }}>
+                    프로필 변경
+                </div>
             </div>
             <div className='MyCategory'>
                 <ul>
-                    <MyPageCategory underLine={underLine} setUnderLine={setUnderLine}/>
-                    <li>후원한 프로젝트</li>
-                    <li>작성한 후기</li>
-                    <li>팔로워</li>
-                    <li>팔로잉</li>
+                    {CateName.map((item, index) => (
+                        <MyPageCategory
+                            key={index}
+                            name={item}
+                            index={index}
+                            selectedIndex={selectedIndex}
+                            setSelectedIndex={setSelectedIndex}
+                        />
+                    ))}
                 </ul>
             </div>
+            <div className='MyPage_Content'> 
+                <p>등록된 {CateName[selectedIndex]}이(가) 없습니다</p> 
+            </div>
         </div>
-    )
+    );
 }
 
 export default MyPage;
--- src/myPage/MyPageCategory.js
+++ src/myPage/MyPageCategory.js
@@ -1,14 +1,20 @@
-import './MyPageCss.css'
 
-function MyPageCategory() {
+import React from 'react';
 
+function MyPageCategory({ name, index, selectedIndex, setSelectedIndex }) {
+    const isSelected = selectedIndex === index;
     return (
-        <>
-            <li className="GreenLine" onClick={() => {
-                setUnderLine(!underLine);
-            }}>프로필</li>
-        </>
-    )
+        <li 
+            onClick={() => setSelectedIndex(index)}
+            style={{
+                borderBottom: isSelected ? '2px solid #40A578' : 'none',
+                cursor: 'pointer',
+                textalign: 'center'
+            }}
+        >
+            {name}
+        </li>
+    );
 }
 
 export default MyPageCategory;
