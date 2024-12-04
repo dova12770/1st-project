b51296b4a4fc89a6400f9781b51814d8dc31b828
kangsun1 <sunkang1224@naver.com>
2024-12-04 10:25:02 +0900

    상세페이지 중간부분 진행

--- src/component/PtcrpgCom2.js
+++ src/component/PtcrpgCom2.js
@@ -21,15 +21,16 @@ function PtcrpgCom2(props) {
                         <div className='expnBody'>이미지및상품소개</div>
                         <div className='expnBody'><img src={props.userImages[0]} width={630} /></div>
                     </div>
-                    <div className='expnRight'>  {/* 상품 상세옵션 선택 박스 map반복 */}
-                        {
-                            props.userPrice.map((item,index) => {
-                                return (
-                                    <PtcrpgExpnRightCom userPrice={item} index={index} userPriceText={props.userPriceText} />
-                                )
-                            })
-                        }
-                    </div>
+                        <div className='expnRight'>  {/* 상품 상세옵션 선택 박스 map반복 */}
+                            {
+                                props.userPrice.map((item, index) => {
+
+                                    return (
+                                        <PtcrpgExpnRightCom userPrice={item} index={index} userPriceText={props.userPriceText} />
+                                    )
+                                })
+                            }
+                        </div>
 
                 </div>
             </div>
--- src/component/PtcrpgExpnRightCom.js
+++ src/component/PtcrpgExpnRightCom.js
@@ -1,8 +1,13 @@
 import '../particularpage/Ptcrpg2.css';
 
 
-function PtcrpgExpnRightCom(props){
-    return(
+
+
+function PtcrpgExpnRightCom(props) {    //오른쪽 가격옵션 선택창 내부에 있는 작은 박스
+
+    
+
+    return (
         <div className='innerExpnRight'>
             <p className='pakagePrice'>{props.userPrice}원</p>
             <p>{props.userPriceText[props.index]}</p>
