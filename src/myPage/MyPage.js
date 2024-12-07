23511353736607e1edc52dbb71262f410d156fc9
dova12770 <aaaa2460@naver.com>
2024-12-07 12:02:18 +0900

    실험용 네비게이트

--- src/myPage/MyPage.js
+++ src/myPage/MyPage.js
@@ -4,10 +4,12 @@ import userImg from '../image/userImg.png';
 import { GoGear } from "react-icons/go";
 import { useState } from 'react';
 import MyPageCategory from './MyPageCategory';
+import {useNavigate} from 'react-router-dom';
 
 function MyPage() {
     const CateName = ['프로필', '후원한 프로젝트', '작성후기', '팔로워', '팔로잉'];
     const [selectedIndex, setSelectedIndex] = useState(0);
+    const navigate = useNavigate();
 
     return (
         <div className='MyPage_Container'>
@@ -16,9 +18,9 @@ function MyPage() {
                     <img src={userImg} alt='유저기본이미지' height='100px' width='100px'/>
                 </div>
                 <div className='UserName'>김도현
-                    <div className='OptionGear'><GoGear /></div>
+                    <div className='OptionGear' onClick={()=> navigate('./MySetting')}><GoGear /></div>
                 </div>
-                <div className='Prof_button' onClick={() => { }}>
+                <div className='Prof_button' onClick={() => navigate('./MySetting')}>
                     프로필 변경
                 </div>
             </div>
