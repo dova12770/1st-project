93771cf6288dd0b38804731096fd3aaefd004e3f
kangsun1 <sunkang1224@naver.com>
2024-12-09 14:03:12 +0900

    상세페이지 navbar 경로설정

--- src/merge/Merge.js
+++ src/merge/Merge.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Header from "../header/Header";
-import Footer from "../footer/Footer";
 import './Merge.css';
 import Body from '../pages/Body';
 import MyPage from '../myPage/MyPage';
 import MySetting from '../myPage/MySetting';
-import PtcrTotal from './PtcrTotal';
 import Ptcrpg from "../particularpage/Ptcrpg";
 import UpdatePage from "../particularpage/pages/UpdatePage";
 import CommuPage from "../particularpage/pages/CommuPage";
@@ -16,14 +14,11 @@ import HeaderTotal from './HeaderTotal';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
 import InterestPage from '../pages/InterestPage';
-<<<<<<< HEAD
 import { useState } from 'react';
 import Rank from '../pages/Rank';
-=======
 import PlanPage from '../particularpage/pages/PlanPage';
 import ScrollToTop from './ScrollToTop';
 
->>>>>>> pcj-router
 
 function Merge() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -35,35 +30,20 @@ function Merge() {
                 <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
                 <Routes>
                     <Route path='/' element={<Body />} />
-<<<<<<< HEAD
-                    <Route path='/ptcr' element={<PtcrTotal />} />                   
+                    <Route path='/ptcr' element={<Ptcrpg />} />
+                    <Route path='/planpage' element={<Ptcrpg />} />
+                    <Route path='/updatepage' element={<UpdatePage />} />
+                    <Route path='/commupage' element={<CommuPage />} />
+                    <Route path='/reviewpage' element={<ReviewPage />} />                
                     <Route path='/login' element={<LoginPage setIsLoggedIn={setIsLoggedIn}/>} /> {/* 로그인 */}
                     <Route path="/signup" element={<SignupPage />} /> {/* 회원가입 */}
                     <Route path='/mypage' element={<MyPage />} /> {/* 마이페이지 */}
                     <Route path='/mysetting' element={<MySetting />} />
                     <Route path="/interest" element={<InterestPage />} /> {/* 선호 */}
                     <Route path='/rank' element={< Rank/>} /> {/* 인기 */}
-
                     {/*<Route path='/' element={< />} /> */} {/* 신규 */}
                     {/*<Route path='/' element={< />} /> */} {/* 공개예정 */}
                     {/*<Route path='/' element={< />} /> */} {/* 마감임박 */}
-=======
-                    <Route path='/ptcr' element={<Ptcrpg />} />
-                    <Route path='/planpage' element={<Ptcrpg />} />
-                    <Route path='/updatepage' element={<UpdatePage />} />
-                    <Route path='/commupage' element={<CommuPage />} />
-                    <Route path='/reviewpage' element={<ReviewPage />} />
-                    <Route path='/login' element={<LoginPage />} />
-                    <Route path='/mypage' element={<MyPage />} />
-                    <Route path='/mysetting' element={<MySetting />} />
-                    <Route path='/signup' element={<SignupPage />} />
-                    <Route path='/interest' element={< InterestPage />} />
-                    {/* <PtcrTotal />
-                                <HeaderTotal />
-                                <MyPage />
-                                <MySetting />
-                                <Body /> */}
->>>>>>> pcj-router
                 </Routes>
                 <MainFooter />
             </div>
