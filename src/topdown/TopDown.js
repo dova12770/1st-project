c31912922fb360610985b8d90d7bde31af472ba4
dova12770 <aaaa2460@naver.com>
2024-12-10 10:36:32 +0900

    수치조정

--- src/topdown/TopDown.js
+++ src/topdown/TopDown.js
@@ -7,7 +7,7 @@ function TopDown() {
     useEffect(() => {
         const handleScroll = () => {
             const td_btn = document.getElementsByClassName('td_btn')[0];
-            if (document.documentElement.scrollTop > 1000) {
+            if (document.documentElement.scrollTop > 800) {
                 td_btn.style.opacity = '1';
             } else {
                 td_btn.style.opacity = '0';
