590359f2a0078d12092d995989917511d04d8ac5
kangsun1 <sunkang1224@naver.com>
2024-12-10 15:19:33 +0900

    데이터 이미지 및 상세페이지 연동

--- src/component/PtcrpgComBottom.js
+++ src/component/PtcrpgComBottom.js
@@ -27,6 +27,12 @@ function PtcrpgComBottom(props) {
                         <div className='expnBody'><img src={props.data[0].userImagesDT[1]} width={630} /></div>
                         <div className='expnBody'>{props.data[0].userTextDT[1]}</div>
                         <div className='expnBody'><img src={props.data[0].userImagesDT[2]} width={630} /></div>
+                        <div className='expnBody'>{props.data[0].userTextDT[2]}</div>
+                        <div className='expnBody'><img src={props.data[0].userImagesDT[3]} width={630} /></div>
+                        <div className='expnBody'>{props.data[0].userTextDT[3]}</div>
+                        <div className='expnBody'><img src={props.data[0].userImagesDT[4]} width={630} /></div>
+                        <div className='expnBody'>{props.data[0].userTextDT[4]}</div>
+                        <div className='expnBody'><img src='/images/배송교환환불.jpg' width={630} /></div>
 
                         <div className='noticeText1'>
                             <div className='noticeTitle'><CgShapeTriangle style={{ fontSize: '18px' }} />신뢰와 안전</div>
--- src/particularpage/data/Datas.js
+++ src/particularpage/data/Datas.js
@@ -5,23 +5,21 @@ let Datas = [
         convergeAmountDT: 100,
         cvgPercentDT: null,
         remainDateDT: null,
-        sponserCountDT: '1',
+        sponserCountDT: '4',
         targetAmountDT: 200,
         fundDate1DT: '2024-11-01',
         fundDate2DT: '2024-12-21',
-        paydayDT: '2024-12-10',
-        goDeliveryDT: '2024-12-20',
+        paydayDT: '2024-12-22',
+        goDeliveryDT: '2024-12-25',
         userPriceDT: ['10,000', '20,000', '30,000'],
         userPriceTextDT: ['생존키트 A형', '생존키트 B형', '생존키트 C형'],
         imagesDT: [
             process.env.PUBLIC_URL + '/images/라이프클락1.jpg',
             process.env.PUBLIC_URL + '/images/라이프클락2.jpg',
-            process.env.PUBLIC_URL + '/images/배송교환환불.jpg'
         ],
         userImagesDT: [
             process.env.PUBLIC_URL + '/images/라이프클락1.jpg',
             process.env.PUBLIC_URL + '/images/라이프클락2.jpg',
-            process.env.PUBLIC_URL + '/images/배송교환환불.jpg'
         ],
         userTextDT: [
             '작고 가벼워서 휴대가 편해요',
@@ -34,32 +32,41 @@ let Datas = [
     {
         idDT: '002',
         ppTitleDT: '방염 담요',
-        convergeAmountDT: '200',
-        cvgPercentDT: '모인금액/목표금액*100',
-        remainDateDT: '마감날짜-현재날짜',
+        convergeAmountDT: 200,
+        cvgPercentDT: null,
+        remainDateDT: null,
         sponserCountDT: '5',
-        targetAmountDT: '1000',
-        fundDate1DT: '2024-5-03',
+        targetAmountDT: 1000,
+        fundDate1DT: '2024-11-03',
         fundDate2DT: '2024-12-25',
-        paydayDT: '2024-6-10',
-        goDeliveryDT: '2024-6-12',
+        paydayDT: '2024-12-26',
+        goDeliveryDT: '2024-12-27',
         userPriceDT: ['15,000', '15,000', '15,000', '16,000', '17,000'],
         userPriceTextDT: ['S 사이즈', 'M 사이즈', 'L 사이즈', 'XL  사이즈', 'XXL 사이즈'],
 
         imagesDT: [
             process.env.PUBLIC_URL + '/images/방염담요1.jpg',
             process.env.PUBLIC_URL + '/images/방염담요2.jpg',
-            process.env.PUBLIC_URL + '/images/배송교환환불.jpg'
+            process.env.PUBLIC_URL + '/images/방염담요3.jpg',
+            process.env.PUBLIC_URL + '/images/방염담요4.jpg',
+            process.env.PUBLIC_URL + '/images/방염담요5.jpg',
+            process.env.PUBLIC_URL + '/images/방염담요6.jpg'
         ],
 
         userImagesDT: [
             process.env.PUBLIC_URL + '/images/방염담요1.jpg',
             process.env.PUBLIC_URL + '/images/방염담요2.jpg',
-            process.env.PUBLIC_URL + '/images/배송교환환불.jpg'
+            process.env.PUBLIC_URL + '/images/방염담요3.jpg',
+            process.env.PUBLIC_URL + '/images/방염담요4.jpg',
+            process.env.PUBLIC_URL + '/images/방염담요5.jpg',
+            process.env.PUBLIC_URL + '/images/방염담요6.jpg'
         ],
         userTextDT: [
             '작고 가벼워서 휴대가 편해요',
-            '성인 남성도 사이즈가 충분해요'
+            '성인 남성도 사이즈가 충분해요',
+            '설명1',
+            '설명2',
+            '설명3'
         ],
         userNameDT: ['김이름2', '이이름2', '박이름2', '최이름2'],
         userCommentDT: ['최고에요!2', '좋아요!2', '그냥그래요2', '별로에요2']
@@ -68,32 +75,222 @@ let Datas = [
     {
         idDT: '003',
         ppTitleDT: '재난생존가방',
-        convergeAmountDT: '500',
-        cvgPercentDT: '모인금액/목표금액*100',
-        remainDateDT: '마감날짜-현재날짜',
-        sponserCountDT: '12',
-        targetAmountDT: '10000',
-        fundDate1DT: '2024-7-07',
-        fundDate2DT: '2024-12-31',
-        paydayDT: '2024-7-25',
-        goDeliveryDT: '2024-7-27',
+        convergeAmountDT: 500,
+        cvgPercentDT: null,
+        remainDateDT: null,
+        sponserCountDT: '32',
+        targetAmountDT: 10000,
+        fundDate1DT: '2024-11-07',
+        fundDate2DT: '2024-12-20',
+        paydayDT: '2024-12-21',
+        goDeliveryDT: '2024-12-23',
         userPriceDT: ['20,000', '30,000', '40,000', '50,000', '60,000'],
         userPriceTextDT: ['1개 세트', '2개 세트', '3개 세트', '4개 세트', '5개 세트'],
 
         imagesDT: [
-            process.env.PUBLIC_URL + '/images/재난생존가방1.jpg',
-            process.env.PUBLIC_URL + '/images/재난생존가방2.jpg',
-            process.env.PUBLIC_URL + '/images/배송교환환불.jpg'
+            process.env.PUBLIC_URL + '/images/생존가방.jpg',
+            process.env.PUBLIC_URL + '/images/생존가방1.jpg',
+            process.env.PUBLIC_URL + '/images/생존가방2.jpg',
+            process.env.PUBLIC_URL + '/images/생존가방3.jpg',
         ],
 
         userImagesDT: [
-            process.env.PUBLIC_URL + '/images/재난생존가방1.jpg',
-            process.env.PUBLIC_URL + '/images/재난생존가방2.jpg',
-            process.env.PUBLIC_URL + '/images/배송교환환불.jpg'
+            process.env.PUBLIC_URL + '/images/생존가방.jpg',
+            process.env.PUBLIC_URL + '/images/생존가방1.jpg',
+            process.env.PUBLIC_URL + '/images/생존가방2.jpg',
+            process.env.PUBLIC_URL + '/images/생존가방3.jpg',
         ],
         userTextDT: [
             '재난 상황시 사용할 물품이 한 가방에 모두있어요',
-            '다양한 물품'
+            '다양한 물품',
+            '유통기한 확인'
+        ],
+        userNameDT: ['김이름3', '이이름3', '박이름3', '최이름3'],
+        userCommentDT: ['최고에요!3', '좋아요!3', '그냥그래요3', '별로에요3']
+    },
+
+    {
+        idDT: '004',
+        ppTitleDT: '구급가방',
+        convergeAmountDT: 1200,
+        cvgPercentDT: null,
+        remainDateDT: null,
+        sponserCountDT: '12',
+        targetAmountDT: 15000,
+        fundDate1DT: '2024-12-01',
+        fundDate2DT: '2025-01-01',
+        paydayDT: '2025-01-02',
+        goDeliveryDT: '2025-01-05',
+        userPriceDT: ['30,000', '40,000', '50,000', '60,000', '70,000'],
+        userPriceTextDT: ['1호 세트', '2호 세트', '3호 세트', '4호 세트', '5호 세트'],
+
+        imagesDT: [
+            process.env.PUBLIC_URL + '/images/구급가방.jpg',
+            process.env.PUBLIC_URL + '/images/구급가방1.jpg',
+            process.env.PUBLIC_URL + '/images/구급가방2.jpg',
+            process.env.PUBLIC_URL + '/images/구급가방3.jpg',
+        ],
+
+        userImagesDT: [
+            process.env.PUBLIC_URL + '/images/구급가방.jpg',
+            process.env.PUBLIC_URL + '/images/구급가방1.jpg',
+            process.env.PUBLIC_URL + '/images/구급가방2.jpg',
+            process.env.PUBLIC_URL + '/images/구급가방3.jpg',
+        ],
+        userTextDT: [
+            '눈에 잘띄는 빨간 디자인!',
+            '들고 다니기 쉽게 손잡이가 있어요!',
+            '16가지의 내용물!',
+            '보관하기 부담없는 사이즈!'
+        ],
+        userNameDT: ['김이름3', '이이름3', '박이름3', '최이름3'],
+        userCommentDT: ['최고에요!3', '좋아요!3', '그냥그래요3', '별로에요3']
+    },
+
+    {
+        idDT: '005',
+        ppTitleDT: '방독면',
+        convergeAmountDT: 1700,
+        cvgPercentDT: null,
+        remainDateDT: null,
+        sponserCountDT: '17',
+        targetAmountDT: 30000,
+        fundDate1DT: '2024-12-05',
+        fundDate2DT: '2025-01-05',
+        paydayDT: '2025-01-06',
+        goDeliveryDT: '2025-01-07',
+        userPriceDT: ['20,000', '20,000', '30,000', '40,000', '50,000'],
+        userPriceTextDT: ['S 사이즈', 'M 사이즈', 'L 사이즈', 'XL  사이즈', 'XXL 사이즈'],
+
+        imagesDT: [
+            process.env.PUBLIC_URL + '/images/방독면.jpg',
+            process.env.PUBLIC_URL + '/images/방독면1.jpg',
+            process.env.PUBLIC_URL + '/images/방독면2.jpg',
+            process.env.PUBLIC_URL + '/images/방독면3.jpg',
+        ],
+
+        userImagesDT: [
+            process.env.PUBLIC_URL + '/images/방독면.jpg',
+            process.env.PUBLIC_URL + '/images/방독면1.jpg',
+            process.env.PUBLIC_URL + '/images/방독면2.jpg',
+            process.env.PUBLIC_URL + '/images/방독면3.jpg',
+        ],
+        userTextDT: [
+            '눈에 잘띄는 노란 디자인!',
+            '오랜기간 보관 가능!',
+            '다양한 장점들!',
+            '가격대비 뛰어난 성능!'
+        ],
+        userNameDT: ['김이름3', '이이름3', '박이름3', '최이름3'],
+        userCommentDT: ['최고에요!3', '좋아요!3', '그냥그래요3', '별로에요3']
+    },
+
+    {
+        idDT: '006',
+        ppTitleDT: '부착식 소화기',
+        convergeAmountDT: 21000,
+        cvgPercentDT: null,
+        remainDateDT: null,
+        sponserCountDT: '12',
+        targetAmountDT: 120000,
+        fundDate1DT: '2024-12-06',
+        fundDate2DT: '2025-01-08',
+        paydayDT: '2025-01-09',
+        goDeliveryDT: '2025-01-11',
+        userPriceDT: ['20,000', '30,000', '40,000', '50,000', '60,000'],
+        userPriceTextDT: ['XS 사이즈', 'S 사이즈', 'M 사이즈', 'L  사이즈', 'XL 사이즈'],
+
+        imagesDT: [
+            process.env.PUBLIC_URL + '/images/부착식소화기.jpg',
+            process.env.PUBLIC_URL + '/images/부착식소화기1.jpg',
+            process.env.PUBLIC_URL + '/images/부착식소화기2.jpg'
+        ],
+
+        userImagesDT: [
+            process.env.PUBLIC_URL + '/images/부착식소화기.jpg',
+            process.env.PUBLIC_URL + '/images/부착식소화기1.jpg',
+            process.env.PUBLIC_URL + '/images/부착식소화기2.jpg'
+        ],
+        userTextDT: [
+            '어디든 부착가능한 초소형 크기!',
+            '크기별로 선택가능!',
+            '누구든 설치할수있는 간편한 부착!'
+        ],
+        userNameDT: ['김이름3', '이이름3', '박이름3', '최이름3'],
+        userCommentDT: ['최고에요!3', '좋아요!3', '그냥그래요3', '별로에요3']
+    },
+
+    {
+        idDT: '007',
+        ppTitleDT: '다막는 차수문',
+        convergeAmountDT: 170000,
+        cvgPercentDT: null,
+        remainDateDT: null,
+        sponserCountDT: '12',
+        targetAmountDT: 500000,
+        fundDate1DT: '2024-12-08',
+        fundDate2DT: '2025-01-10',
+        paydayDT: '2025-01-11',
+        goDeliveryDT: '2025-01-15',
+        userPriceDT: ['20,000', '40,000', '55,000', '60,000', '70,000'],
+        userPriceTextDT: ['1개 세트', '2개 세트', '3개 세트', '4개 세트', '5개 세트'],
+
+        imagesDT: [
+            process.env.PUBLIC_URL + '/images/차수문.jpg',
+            process.env.PUBLIC_URL + '/images/차수문2.jpg',
+            process.env.PUBLIC_URL + '/images/차수문3.jpg'
+        ],
+
+        userImagesDT: [
+            process.env.PUBLIC_URL + '/images/차수문.jpg',
+            process.env.PUBLIC_URL + '/images/차수문2.jpg',
+            process.env.PUBLIC_URL + '/images/차수문3.jpg'
+        ],
+        userTextDT: [
+            '내구성 강한 티타늄 재질!',
+            '매우 간편한 설치!',
+            '실사용 예시'
+        ],
+        userNameDT: ['김이름3', '이이름3', '박이름3', '최이름3'],
+        userCommentDT: ['최고에요!3', '좋아요!3', '그냥그래요3', '별로에요3']
+    },
+
+    {
+        idDT: '008',
+        ppTitleDT: '지진대피 방재모자',
+        convergeAmountDT: 20000,
+        cvgPercentDT: null,
+        remainDateDT: null,
+        sponserCountDT: '1',
+        targetAmountDT: 500000,
+        fundDate1DT: '2024-12-10',
+        fundDate2DT: '2025-01-25',
+        paydayDT: '2025-01-26',
+        goDeliveryDT: '2025-01-28',
+        userPriceDT: ['20,000', '25,000', '30,000'],
+        userPriceTextDT: ['S 사이즈', 'M 사이즈', 'L사이즈'],
+
+        imagesDT: [
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자.jpg',
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자1.jpg',
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자2.jpg',
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자3.jpg',
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자4.jpg',
+        ],
+
+        userImagesDT: [
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자.jpg',
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자1.jpg',
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자2.jpg',
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자3.jpg',
+            process.env.PUBLIC_URL + '/images/지진대피 방재모자4.jpg',
+        ],
+        userTextDT: [
+            '눈에 잘띄는 노란색 디자인!',
+            '화재에도 강한 재질과 낙하물에 의한 충격을 흡수하는 두꺼운재질!',
+            '',
+            '접을 수 있어 휴대가 간편합니다!',
+            '세계 최초 신소제 난연메모리 원단 100%!'
         ],
         userNameDT: ['김이름3', '이이름3', '박이름3', '최이름3'],
         userCommentDT: ['최고에요!3', '좋아요!3', '그냥그래요3', '별로에요3']
